a4c4763f234ec490c967ecaa7f998a55fd411216
martin <martinjdospel@gmail.com>
2022-10-27 21:52:59 +0200

    modified index.js, added globals.scss, created components placeholders categories, postcard, postwidget

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,25 @@
-import Head from "next/head";
+import { PostCard, Categories, PostWidget } from "../components";
 
 const posts = [
-  {title: "Post title", excerpt: "post excerpt"},
-  {title: "Second post title", excerpt: "second post excerpt"}
-]
+  { title: "Post title", excerpt: "post excerpt" },
+  { title: "Second post title", excerpt: "second post excerpt" },
+];
 
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        {posts.map((p, i) => (
-          <div key="{item}">
-            {p.title}
-            {p.excerpt}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post) => (
+            <PostCard key={post} post={post} />
+          ))}
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
           </div>
-        ))}
+        </div>
       </div>
     </div>
   );
