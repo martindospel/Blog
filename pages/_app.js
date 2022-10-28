e23a630452651d6a0155663539e2a2234251d90f
martin <martinjdospel@gmail.com>
2022-10-28 12:10:11 +0200

    graphql query done, layout component and header created, background and overlay added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.scss';
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
+
+import React, { useEffect, useState } from "react";
+import { Layout } from "../components";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Component {...pageProps} />
+    <div id="overlay">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
     <div className="container mx-auto px-10 mb-8">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post) => (
-            <PostCard key={post} post={post} />
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
