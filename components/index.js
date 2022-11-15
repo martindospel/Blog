30c292765f69242331246ca3039995a4d00773d9
martin <martinjdospel@gmail.com>
2022-11-15 23:56:01 +0100

    created author, comments, post details components, post slug, changed overlay to make it cover entire page

--- components/index.js
+++ components/index.js
@@ -3,3 +3,29 @@ export { default as Layout } from './Layout';
 export { default as Categories } from './Categories';
 export { default as PostWidget } from './PostWidget';
 export { default as Header } from './Header';
+export { default as Comments } from './Comments';
+export { default as CommentsForm } from './CommentsForm';
+export { default as Author } from './Author';
+export { default as PostDetail } from './PostDetail';
+
+// import PostCard from "./PostCard";
+// import Layout from "./Layout";
+// import Categories from "./Categories";
+// import PostWidget from "./PostWidget";
+// import Header from "./Header";
+// import Comments from "./Comments";
+// import CommentsForm from "./CommentsForm";
+// import Author from "./Author";
+// import PostDetail from "./PostDetail";
+
+// export {
+//   PostCard,
+//   PostDetail,
+//   PostWidget,
+//   Layout,
+//   Categories,
+//   Header,
+//   Comments,
+//   CommentsForm,
+//   Author,
+// };
