3fe28b85de50fe2b32cc7709a54ee19d49ff2672
martin <martinjdospel@gmail.com>
2022-11-17 23:26:50 +0100

    generic excerpts added

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
         <div className="md:float-left block">
           <Link href="/">
             <span className="cursor-pointer font-bold text-4xl text-white">
-              Martin Dospel's blog
+              Blog
             </span>
           </Link>
         </div>
